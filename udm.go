6f0e042f75ebaa7e43ee3cb80dfb629f457b7ccb
Bryan Morrow <bkmorrow@gmail.com>
2022-08-17 11:23:04 -0500

    securityResult slice

--- udm.go
+++ udm.go
@@ -3,14 +3,14 @@ package udm
 type UDM struct {
 	About *Noun `json:"about,omitempty"` // Repeatable
 	// Additional 	protobuf	`json:"additional"`
-	Extensions     *Extensions     `json:"extensions,omitempty"`
-	Intermediary   *Noun           `json:"intermediary,omitempty"` // Repeatable
-	Metadata       Metadata        `json:"metadata"`
-	Network        *Network        `json:"network,omitempty"`
-	Observer       *Noun           `json:"observer,omitempty"`
-	Principal      *Noun           `json:"principal,omitempty"`
-	SecurityResult *SecurityResult `json:"security_result,omitempty"` // Repeatable
-	Src            *Noun           `json:"src,omitempty"`
-	Target         *Noun           `json:"target,omitempty"`
-	Vulnerability  *Vulnerability  `json:"vulnerability,omitempty"`
+	Extensions     *Extensions       `json:"extensions,omitempty"`
+	Intermediary   *Noun             `json:"intermediary,omitempty"` // Repeatable
+	Metadata       Metadata          `json:"metadata"`
+	Network        *Network          `json:"network,omitempty"`
+	Observer       *Noun             `json:"observer,omitempty"`
+	Principal      *Noun             `json:"principal,omitempty"`
+	SecurityResult []*SecurityResult `json:"security_result,omitempty"` // Repeatable
+	Src            *Noun             `json:"src,omitempty"`
+	Target         *Noun             `json:"target,omitempty"`
+	Vulnerability  *Vulnerability    `json:"vulnerability,omitempty"`
 }
