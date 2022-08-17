cbe7cf84f82796862615c62b225ac081d6643615
Bryan Morrow <bkmorrow@gmail.com>
2022-08-17 11:32:42 -0500

    more pointers

--- extensions.go
+++ extensions.go
@@ -2,6 +2,6 @@ package udm
 
 // Extensions - Event types with first-class metadata that are not already categorized by the Chronicle UDM.
 type Extensions struct {
-	Auth  Authentication `json:"auth,omitempty"`  // Extension to the authentication metadata.
-	Vulns Vulnerability  `json:"vulns,omitempty"` // Extension to the vulnerability metadata.
+	Auth  *Authentication `json:"auth,omitempty"`  // Extension to the authentication metadata.
+	Vulns *Vulnerability  `json:"vulns,omitempty"` // Extension to the vulnerability metadata.
 }
