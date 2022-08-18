2094f9362e59d4d307c0c8ac4489104a18205ce1
Bryan Morrow <bkmorrow@gmail.com>
2022-08-18 11:12:48 -0500

    metadata ingestionlabels as slice

--- metadata.go
+++ metadata.go
@@ -89,18 +89,19 @@ const (
 
 // Metadata - The event metadata section for UDM events stores general information about each event.
 type Metadata struct {
-	CollectedTimestamp *time.Time `json:"collected_timestamp,omitempty"` // Collection time of the event RFC 3339
-	Description        string     `json:"description,omitempty"`         // Human-readable description of the event 1024-byte max
-	EventTimestamp     *time.Time `json:"event_timestamp"`               // Event generation time RFC 3339
-	EventType          string     `json:"event_type"`                    // Specifies the primary type of the event
-	ID                 []byte     `json:"id,omitempty"`
-	IngestedTimestamp  *time.Time `json:"ingested_timestamp,omitempty"`
-	IngestionLabels    *Label     `json:"ingestion_labels,omitempty"`
-	ProductEventType   string     `json:"product_event_type,omitempty"` // Short, descriptive, human-readable, and product-specific event name or type. 64-byte max
-	ProductLogID       string     `json:"product_log_id,omitempty"`     // Encodes a vendor-specific event identifier to uniquely identify the event (a GUID). case-sensitive 256-byte max
-	ProductName        string     `json:"product_name,omitempty"`       // Specifies the name of the product. 256-byte max
-	ProductVersion     string     `json:"product_version,omitempty"`    // Specifies the version of the product. 32-byte max
-	Tags               *Tag       `json:"tags,omitempty"`
-	URLBackToProduct   string     `json:"url_back_to_product,omitempty"` // URL linking to a relevant website where you can view more information - Valid RFC 3986 URL
-	VendorName         string     `json:"vendor_name,omitempty"`         // Specifies the product vendor's name - 256-byte max
+	CollectedTimestamp  *time.Time `json:"collected_timestamp,omitempty"` // Collection time of the event RFC 3339
+	Description         string     `json:"description,omitempty"`         // Human-readable description of the event 1024-byte max
+	EventTimestamp      *time.Time `json:"event_timestamp"`               // Event generation time RFC 3339
+	EventType           string     `json:"event_type"`                    // Specifies the primary type of the event
+	ID                  []byte     `json:"id,omitempty"`
+	IngestedTimestamp   *time.Time `json:"ingested_timestamp,omitempty"`
+	IngestionLabels     []*Label   `json:"ingestion_labels,omitempty"`
+	ProductDeploymentID string     `json:"product_deployment_id,omitempty"` //The deployment identifier assigned by the vendor for a product deployment.
+	ProductEventType    string     `json:"product_event_type,omitempty"`    // Short, descriptive, human-readable, and product-specific event name or type. 64-byte max
+	ProductLogID        string     `json:"product_log_id,omitempty"`        // Encodes a vendor-specific event identifier to uniquely identify the event (a GUID). case-sensitive 256-byte max
+	ProductName         string     `json:"product_name,omitempty"`          // Specifies the name of the product. 256-byte max
+	ProductVersion      string     `json:"product_version,omitempty"`       // Specifies the version of the product. 32-byte max
+	Tags                *Tag       `json:"tags,omitempty"`
+	URLBackToProduct    string     `json:"url_back_to_product,omitempty"` // URL linking to a relevant website where you can view more information - Valid RFC 3986 URL
+	VendorName          string     `json:"vendor_name,omitempty"`         // Specifies the product vendor's name - 256-byte max
 }
